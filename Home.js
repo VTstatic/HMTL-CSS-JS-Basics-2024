2ee5540d7094796b46c1eeefb8c1ce213c4f4afc
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-12 13:03:52 +0800

    Episode 6 Random Number on Button

--- Home.js
+++ Home.js
@@ -1,16 +1,40 @@
-let count = 0;
+//let x = Math.floor(Math.random() * 20) + 1;
+//let y = Math.floor(Math.random() * 100) + 1;
+//let z = Math.floor(Math.random() * 1000) + 1;
 
-document.getElementById("decreaseBtn").onclick = function(){
-    count-=1;
-    document.getElementById("countlabel").innerHTML = count;
+let a;
+let b;
+let c;
+
+document.getElementById("firstBtn").onclick = function(){
+    a = Math.floor(Math.random() * 20);
+    document.getElementById("countlabel1").innerHTML = a;
+}
+
+document.getElementById("secondBtn").onclick = function(){
+    b = Math.floor(Math.random() * 100);
+    document.getElementById("countlabel2").innerHTML = b;
+}
+
+document.getElementById("thirdBtn").onclick = function(){
+    c = Math.floor(Math.random() * 1000);
+    document.getElementById("countlabel3").innerHTML = c;
+} 
+
+document.getElementById("rollBtn").onclick = function(){
+    a = Math.floor(Math.random() * 20) + 1;
+    b = Math.floor(Math.random() * 100) + 1;
+    c = Math.floor(Math.random() * 1000) + 1;
+    document.getElementById("countlabel1").innerHTML = a;
+    document.getElementById("countlabel2").innerHTML = b;
+    document.getElementById("countlabel3").innerHTML = c;
 }
 
 document.getElementById("resetBtn").onclick = function(){
-    count=0;
-    document.getElementById("countlabel").innerHTML = count;
-}
-
-document.getElementById("increaseBtn").onclick = function(){
-    count+=1;
-    document.getElementById("countlabel").innerHTML = count;
+    a = 0;
+    b = 0;
+    c = 0;
+    document.getElementById("countlabel1").innerHTML = a;
+    document.getElementById("countlabel2").innerHTML = b;
+    document.getElementById("countlabel3").innerHTML = c;
 }
