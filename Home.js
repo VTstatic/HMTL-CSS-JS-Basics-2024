f8119e011e455111e8fee11bd19e6699cc7742ca
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-12 13:23:53 +0800

    Episode 7 String Manipulation

--- Home.js
+++ Home.js
@@ -1,40 +1,19 @@
-//let x = Math.floor(Math.random() * 20) + 1;
-//let y = Math.floor(Math.random() * 100) + 1;
-//let z = Math.floor(Math.random() * 1000) + 1;
+//string properties & methods
 
-let a;
-let b;
-let c;
+let username = "Vladimir";
+let nagan = "  Vladimir  ";
+let telepon = "+63-956-810-4011";
 
-document.getElementById("firstBtn").onclick = function(){
-    a = Math.floor(Math.random() * 20);
-    document.getElementById("countlabel1").innerHTML = a;
-}
+console.log(username.length);
+console.log(username.charAt());
+console.log(username.indexOf("r"));
+console.log(username.lastIndexOf("V"));
 
-document.getElementById("secondBtn").onclick = function(){
-    b = Math.floor(Math.random() * 100);
-    document.getElementById("countlabel2").innerHTML = b;
-}
+nagan = nagan.trim(); //trim spaces
+//nagan = nagan.toUpperCase();
+//nagan = nagan.toLowerCase();
+console.log(nagan);
 
-document.getElementById("thirdBtn").onclick = function(){
-    c = Math.floor(Math.random() * 1000);
-    document.getElementById("countlabel3").innerHTML = c;
-} 
-
-document.getElementById("rollBtn").onclick = function(){
-    a = Math.floor(Math.random() * 20) + 1;
-    b = Math.floor(Math.random() * 100) + 1;
-    c = Math.floor(Math.random() * 1000) + 1;
-    document.getElementById("countlabel1").innerHTML = a;
-    document.getElementById("countlabel2").innerHTML = b;
-    document.getElementById("countlabel3").innerHTML = c;
-}
-
-document.getElementById("resetBtn").onclick = function(){
-    a = 0;
-    b = 0;
-    c = 0;
-    document.getElementById("countlabel1").innerHTML = a;
-    document.getElementById("countlabel2").innerHTML = b;
-    document.getElementById("countlabel3").innerHTML = c;
-}
+//replace characters
+telepon = telepon.replaceAll("-", " ")
+console.log(telepon);
