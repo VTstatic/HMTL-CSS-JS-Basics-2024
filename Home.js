ace51fcfb07a87b153ef839613a15b92ca1f6574
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-16 13:22:00 +0800

    Episode 10 if Statement

--- Home.js
+++ Home.js
@@ -1,8 +1,29 @@
-//method chaining = calling one method after another
-//                  in one continuous line of code
+//let age;
+let online = false;
 
-let userName = "Vladimir";
+/*
+a = window.prompt("How old are you?");
+age = a;
 
-let letter = userName.charAt(0).toUpperCase().trim();
 
-console.log(letter);
+if(age >= 65){
+    console.log("you are a getting old!");
+    count = String("you are a getting old!");
+    document.getElementById("slbl1").innerHTML = count;
+} else if(age >= 18) {
+    console.log("you are an adult!");
+    count = String("you are an adult!");
+    document.getElementById("slbl1").innerHTML = count;
+} else {
+    console.log("you're still a CHILD!");
+    count = String("you're still a CHILD!");
+    document.getElementById("slbl1").innerHTML = count;
+} 
+*/
+
+if(online){
+    console.log("You are Online")
+    } else {
+        console.log("You are offline");
+    }
+
